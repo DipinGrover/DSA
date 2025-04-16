98548a458f9155ea3561bf3a7e7668a1f9710b99
Dipin Grover <grover.dipin007@gmail.com>
2025-04-17 02:17:20 +0530

    added deletion of head , deletion of tail , deletion of kth node

--- Linked List/2.cpp
+++ Linked List/2.cpp
@@ -56,30 +56,106 @@ void traversal_in_ll(Node* head)
 // deletion of node in ll
 
 
-// 1. deletion of head
+
+
+// 1. deletion of head in ll
+
 Node* delete_head_of_ll(Node* head)
 {
     if(head == NULL)
     {
-        // if empty ll
+        // empty ll
         return NULL;
     }
 
-    if(head->next == nullptr)
+    if(head->next == NULL)
     {
-        // single element ll and that node will be the head
+        // single element ll and that element will be the head
         delete head;
-
         return NULL;
     }
 
-    Node* temp = head; // temp points to head
+    Node* temp = head; // temp pointing towards head
 
-    head = head->next; // move the head
+    head = head->next; // move head to next
 
     delete temp;
 
     return head;
+
+}
+
+
+// 2. delete tail
+
+Node* delete_tail_of_ll(Node* head)
+{
+    if(head == NULL)
+    {
+        // empty ll 
+        return NULL;
+    }
+
+    if(head->next == NULL)
+    {
+        // single node that will be head as well as tail of ll
+        delete head;
+        return NULL;
+    }
+
+    Node* temp = head;
+
+    while(temp->next->next != nullptr)
+    {
+        temp = temp->next;
+    }
+
+    delete temp->next; // delete temp next
+
+    temp->next = nullptr; // now temp->next is nullptr
+
+    return head; // return head
+}
+
+
+// 3. delete kth node of ll
+
+Node* delete_kth_node(Node* head , int k)
+{
+    if(head == NULL)
+    {
+        // empty ll
+        return NULL;
+    }
+
+    if(k==1)
+    {
+        // delete head of ll
+        head = delete_head_of_ll(head);
+        return head;
+    }
+
+    int count = 0;
+    Node* temp = head;
+    Node* back = NULL;
+
+    while(temp != NULL)
+    {
+        count++;
+        if(count == k)
+        {
+            // this is the node we wanted to delete  
+
+            back->next = temp->next;
+            delete temp;
+            break;
+        }
+        back = temp;
+        temp = temp->next;
+    }
+
+    return head;
+
 }
 
 int main()
@@ -99,8 +175,32 @@ int main()
 
 
 
-    // for deletion of head in ll
-    head = delete_head_of_ll(head);
+    // // for deletion of head in ll
+    // head = delete_head_of_ll(head);
+
+    // cout << endl;
+
+    // traversal_in_ll(head);
+
+
+
+
+
+    // // for deletion of tail in ll
+    // head = delete_tail_of_ll(head);
+
+    // cout << endl;
+
+    // traversal_in_ll(head);
+
+
+
+
+
+    // for deletion of kth node
+    head = delete_kth_node(head , 7);
+
     cout << endl;
+
     traversal_in_ll(head);
 }
